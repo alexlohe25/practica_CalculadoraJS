7414830baf9d3211ecdb036bdf6d2109324012e3
alexlohe25 <A01733984@itesm.mx>
2021-05-03 11:05:54 -0500

    calculadora con APIs

--- main.js
+++ main.js
@@ -1,72 +1,14 @@
-const btnSuma = document.querySelector('.btnSuma');
-const btnResta = document.querySelector('.btnResta');
-const btnMulti = document.querySelector('.btnMulti');
-const btnDiv = document.querySelector('.btnDiv');
-const btnMod = document.querySelector('.btnMod');
-const btnPow = document.querySelector('.btnPow');
-
-function suma(a,b){
-    return a + b;
+function calcula(operacion){
+    var num1 = $('#num1').val();
+    var num2 = $('#num2').val();
+    console.log(num1);
+    console.log(num2);
+    var url = "http://localhost:8085/"+ operacion +"/?num1=" + num1 + "&num2=" + num2;
+    
+    $.getJSON(url,
+        function(json) {
+            console.log(json);
+            window.alert("Resultado: " + json.result)
+        }
+    );
 }
-function resta(a,b){
-    return a - b;
-}
-function multiplicacion(a,b){
-    return a * b;
-}
-function division(a,b){
-    return a / b;
-}
-function mod(a,b){
-    return a % b;
-}
-function exp(a,b){
-    return a ** b;
-}
-btnSuma.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = suma(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
-
-btnResta.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = resta(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
-
-btnMulti.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = multiplicacion(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
-
-btnDiv.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = division(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
-
-btnMod.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = mod(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
-
-btnPow.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = parseInt(document.getElementById("num1").value,10);
-    const b = parseInt(document.getElementById("num2").value,10);
-    const c = exp(a,b);
-    document.getElementById("result").innerHTML = "Resultado: " + c;
-});
